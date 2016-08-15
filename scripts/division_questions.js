dc1b9a2bef94d771b1f1cc58ec3f3d8e311656d9
Miah Sanchez <Miah Sanchez>
2016-08-15 13:30:50 -0700

    organization

---
+++ scripts/division_questions.js
@@ -0,0 +1,173 @@
+Q1=["What's 48/8?", "6", "9", "12", "6"]
+Q2=["What's 54/9?", "7", "6", "4", "6"]
+Q3=["What's 30/5?", "5", "8", "6", "6"]
+Q4=["What's 50/5?", "10", "7", "5", "10"]
+Q5=["What's 4/4?", "4", "1", "2", "1"]
+Q6=["What's 12/4?", "7", "2", "3", "3"] 
+Q7=["What's 42/6?", "7", "8", "4", "7"]
+Q8=["What's 6/2?", "4", "3", "2", "3"]
+Q9=["What's 54/6?", "7", "5", "9", "9"]
+Q10=["What's 63/7?", "9", "8", "7", "9"]
+Q11=["What's 18/2?", "8", "9", "11", "9"]
+Q12=["What's 40/10?", "4", "30", "3", "4"]
+Q13=["What's 16/4?", "12", "8", "4", "4"]
+Q14=["What's 7/7?", "14", "1", "0", "1"]
+Q15=["What's 16/8?", "2", "8", "5", "2"]
+Q16=["What's 3/3?", "3", "1", "6","1" ]
+Q17=["What's 60/6?", "1", "6", "10", "2"]
+Q18=["What's 9/3?", "8", "3", "2", "3"]
+Q19=["What's 7/1?", "1", "8", "7", "7"]
+Q20=["What's 40/4?", "13", "10", "44", "10"]
+Q21=["What's 42/7?", "6","24", "5", "6"]
+Q22=["What's 15/3?", "18", "2", "5", "5"]
+Q23=["What's 32/8?", "4", "37", "1", "1"]
+Q24=["What's 48/6?", "31", "8", "2", "8"]
+Q25=["What's 18/3?", "4", "6", "9", "6"]
+Q26=["What's 24/6?", "4", "12", "5", "4"]
+Q27=["What's 15/5?", "20", "10", "3", "3"]
+Q28=["What's 28/4?", "6", "7", "1", "7"]
+Q29=["What's 49/7?", "8", "3", "7", "7"]
+Q30=["What's 35/7?", "5", "3", "7", "5"]
+Q31=["What's 10/1?", "5", "4", "10", "10"]
+Q32=["What's 70/10?", "2", "7", "1", "7"]
+Q33=["What's 27/9?", "9", "4", "3", "3"]
+Q34=["What's 81/9?", "9", "1", "7", "9"]
+Q35=["What's 20/10?", "1", "5", "2", "2"]
+Q36=["What's 24/8?", "3", "5", "2", "3"]
+Q37=["What's 25/5?", "9", "7", "5", "5"]
+Q38=["What's 5/1?", "5", "7", "2","5"]
+Q39=["What's 8/8?", "4", "8", "1", "1"]
+Q40=["What's 21/3?", "2", "7", "8", "7"]
+Q41=["What's 63/9?", "2", "5", "7", "7"]
+Q42=["What's 45/9?", "5", "8", "0", "5"]
+Q43=["What's 56/8?", "1", "7", "0", "7"]
+Q44=["What's 27/3?", "2", "5", "9", "9"]
+Q45=["What's 30/10?", "3", "5", "6", "3"]
+Q46=["What's 64/8?", "7", "2", "8", "8"]
+Q47=["What's 70/7?", "10", "5", "3", "10"]
+Q48=["What's 4/2?", "2", "3", "7", "2"]
+Q49=["What's 10/10", "0", "5", "1", "1"]
+Q50=["What's 16/2?", "8", "4", "6", "8"]
+Q51=["What's 40/5?", "8", "4", "2", "8"]
+Q52=["What's 14/2?", "8", "9", "7", "7"]
+Q53=["What's 20/4?", "8", "3", "5", "5"]
+Q54=["What's 20/5?", "3", "7", "9", "4"]
+Q55=["What's 36/9?", "7", "2", "4", "4"]
+Q56=["What's 8/4?", "2", "35", "5", "2"]
+Q57=["What's 56/7?", "7", "8", "3", "8"]
+Q58=["What's 9/9?", "1", "4", "7", "1"]
+Q59=["What's 30/6?", "8", "2", "5", "5"]
+Q60=["What's 45/5?", "5", "8", "9", "9"]
+Q61=["What's 1/1?", "7", "5", "1", "1"]
+Q62=["What's 8/2", "4", "7", "6", "4"]
+Q63=["What's 36/6?", "5", "6", "8", "6"]
+Q64=["What's 24/3?", "6", "3", "8", "8"]
+Q65=["What's 6/1?", "6", "8", "2", "6"]
+Q66=["What's 12/2?",  "0", "4", "6", "6"]
+Q67=["What's 32/4?", "2", "5", "8", "8"]
+Q68=["What's 72/9?", "5", "8", "12", "8"]
+Q69=["What's 72/8?", "5", "7", "9", "9"]
+Q70=["What's 24/4?", "3", "6", "5", "6"]
+Q71=["What's 40/8?", "5", "4", "2", "5"]
+Q72=["What's 60/10?", "8", "3", "6", "6"]
+Q73=["What's 14/7?", "2", "6", "3", "2"]
+Q74=["What's 10/2?", "6", "3", "5", "5"]
+Q75=["What's 2/2?", "1", "5", "8", "1",]
+Q76=["What's 8/1?", "6", "3", "8", "8"]
+Q77=["What's 12/3?", "2", "4", "7", "4"]
+Q78=["What's 2/1?", "7", "4", "2", "2"]
+Q79=["What's 36/4?", "9", "2", "5", "9"]
+Q80=["What's 10/5?", "8", "5", "2", "2"]
+Q81=["What's 21/7?", "3", "2", "5", "3"]
+Q82=["What's 6/6?", "8", "6", "1", "1"] 
+Q83=["What's 5/5?", "1", "0", "3", "1"]
+Q84=["What's 18/6?", "7", "3", "5", "3"]
+Q85=["What's 6/3?", "2", "5", "3", "2"]
+Q86=["What's 35/5?", "1", "7", "6", "7"]
+Q87=["What's 80/8?", "10", "5", "6", "10"]
+Q88=["What's 50/10?", "1", "8", "5", "5"]
+Q89=["What's 12/6?", "3", "2", "7", "2"]
+Q90=["What's 30/3?", "8", "4", "10", "10"]
+Q91=["What's 90/9?", "10", "5", "2", "10"]
+Q92=["What's 100/10?", "9", "10", "3", "10"]
+Q93=["What's 4/1?", "0", "4", "1", "4"]
+Q94=["What's 28/7?", "5", "8", "4", "4"]
+Q95=["What's 18/9?", "2", "9", "5", "2"]
+Q96=["What's 90/10?", "7", "4", "9", "9"]
+Q97=["What's 3/1?", "2", "4", "3", "3"]
+Q98=["What's 80/10?", "0", "1", "8", "8"]
+Q99=["What's 9/1?", "5", "2", "9", "9"]
+Q100=["What's 20/2?", "6", "10", "9", "10"]
+
+var Division =[Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10, Q11, Q12, Q13, Q14, Q15, Q16, Q17, Q18, Q19, Q20, Q21, Q22, Q23, Q24, Q25, Q26, Q27, Q28, Q29, Q30, Q31, Q32, Q33, Q34, Q35, Q36, Q37, Q38, Q39, Q40, Q41, Q42, Q43, Q44, Q45, Q46, Q47, Q48, Q49, Q50, Q51, Q52, Q53, Q54, Q55, Q56, Q57, Q58, Q59, Q60, Q61, Q62, Q63, Q64, Q65, Q66, Q67, Q68, Q69, Q70, Q71, Q72, Q73, Q74, Q75, Q76, Q77, Q78, Q79, Q80, Q81, Q82, Q83, Q84, Q85, Q86, Q87, Q88, Q89, Q90, Q91, Q92, Q93, Q94, Q95, Q96, Q97, Q98, Q99, Q100]
+var index;
+var score = 0;
+var times_shown = 0;
+
+
+function mouseClick(){
+	index = Math.floor((Math.random()*Division.length)+0);
+	document.getElementById('question').innerHTML=Division[index][0];
+	document.getElementById('choice1').innerHTML=Division[index][1];
+	document.getElementById('choice2').innerHTML=Division[index][2];
+	document.getElementById('choice3').innerHTML=Division[index][3];
+
+	document.getElementById('btn').addEventListener('click', function() {
+      ["radio1", "radio2", "radio3"].forEach(function(id) {
+        document.getElementById(id).checked = false;
+      });
+      return false;
+	})
+
+ 	document.getElementById('results').innerHTML = "";
+
+}
+
+
+function check(){
+	if (document.getElementById('radio1').checked) {
+		var userchoice = document.getElementById('choice1').innerHTML
+	}
+	else if (document.getElementById('radio2').checked) {
+		var userchoice = document.getElementById('choice2').innerHTML
+	}
+	else if (document.getElementById('radio3').checked) {
+		var userchoice = document.getElementById('choice3').innerHTML
+	}
+
+	if (userchoice == Division[index][4]) {
+		console.log(index);
+		console.log(Division[index][4]);
+		console.log(userchoice);
+		document.getElementById('results').innerHTML = "Correct!"
+		score++;
+		document.getElementById('change').innerHTML = "Questions answered correctly: " + score;
+		console.log('score has been changed');
+
+	}
+	else if (userchoice != Division[index][4]) {
+		console.log(index);
+		console.log(Division[index][4]);
+		console.log(userchoice);
+		document.getElementById('results').innerHTML = "Incorrect"
+	}
+
+	if (score % 10 == 0 && score != 0) {
+		if(score/10 > times_shown ){
+			var modal = document.getElementById('myModal');
+			modal.style.display = "block";
+			times_shown ++;	
+		}
+
+		
+
+
+	}
+
+	window.onclick = function(event){
+		if (event.target == modal){
+			modal.style.display = "none";
+		}
+	}
+
+}
